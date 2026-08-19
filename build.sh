0298ab7beda51274999ef7852a28ac7aad08232c
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-08-19 16:10:29 +0330

    feat: enhance build script by adding installation of build tools, Rust, and wasm-pack for improved Rust environment setup

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+# Install build tools
+apt-get update && apt-get install -y build-essential
+
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+
+# Install wasm-pack
 cargo install wasm-pack
 
+# Build
 bun install
 bun run build
